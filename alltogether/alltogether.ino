43313e27ef22739a2c30d49322307dcd2156507f
RolandStrohmer <111423535+RolandStrohmer@users.noreply.github.com>
2023-05-05 08:59:36 +0200

    Update alltogether.ino

--- alltogether/alltogether.ino
+++ alltogether/alltogether.ino
@@ -95,16 +95,13 @@ void loop() {
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
+
   // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  if (isnan(h) || isnan(t)) {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
 
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
   // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
 
@@ -112,14 +109,9 @@ void loop() {
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
   Serial.print(t);
-  Serial.print(F("°C "));
-  Serial.print(f);
-  Serial.print(F("°F  Heat index: "));
+  Serial.print(F("°C  Heat index: "));
   Serial.print(hic);
-  Serial.print(F("°C "));
-  Serial.print(hif);
-  Serial.println(F("°F"));
-  Serial.println("Connecting...");
+  Serial.println(F("°C "));
 
   if (conn.connect(server_addr, 3306, user, password)) {
     delay(1000);
